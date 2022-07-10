a95436bd69cf9b43c44e78150fd80f98eea323c6
Aline Pollis <aline.pollis@hotmail.com>
2022-07-09 21:47:32 -0300

    Implementação do movie service

--- movie-service/src/test/java/com/lipollis/movieservice/MovieServiceApplicationTests.java
+++ movie-service/src/test/java/com/lipollis/movieservice/MovieServiceApplicationTests.java
@@ -2,6 +2,7 @@ package com.lipollis.movieservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootTest
 class MovieServiceApplicationTests {
